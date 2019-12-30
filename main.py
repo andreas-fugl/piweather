e6b8e2874a46d2890005e1ce62cc5cb7a7410352
Andreas Fugl <peetwo@gmail.com>
2019-12-30 18:54:05 +0100

    implemented mqtt callbacks

--- main.py
+++ main.py
@@ -24,13 +24,33 @@ def visitAllChildren(node):
 
 
 class PiWeatherApp(App):
-    mqttc = mqtt.Client(client_id="PiWeatherApp")
+    def on_message_callback(client, userdata, message):
+        print("PiWeatherApp::on_message_callback")
+
+    def on_connect(client, userdata, flags, rc):
+        print("PiWeatherApp::on_connect rc=%s", str(rc))
+
+    def on_disconnect(mqttc, userdata, rc):
+        if rc != 0:
+            print("Unexpected disconnection. Reconnecting...")
+            # mqttc.reconnect()
+        else:
+            print("Disconnected successfully")
+
+    mqttc = mqtt.Client(client_id="PiWeatherApp2")
+
+    # setup callbacks
+    mqttc.on_connect = on_connect
+    mqttc.on_disconnect = on_disconnect
+    mqttc.on_message = on_message_callback
+
     mqttc.connect("192.168.123.26")
     mqttc.loop_start()
     mqttc.subscribe("debugData")
 
     def say_hello(self):
-        node = visitAllChildren(self.root)
+        # node = visitAllChildren(self.root)
+        print(self.mqttc.is_connected())
 
 
 if __name__ == '__main__':
